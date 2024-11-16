6c452493267e51532829931b3ba11c978e98a262
Xcy0512 <119501248+Xcy0512@users.noreply.github.com>
2024-11-16 15:27:36 +0800

    Add files via upload

---
+++ nn/extra_modules/block.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+import math
+import numpy as np
+from einops import rearrange
+from ..modules.conv import Conv, DWConv, RepConv, GhostConv, autopad
+from ..modules.block import *
+from timm.layers import CondConv2d
+
+__all__ = ['DPEM']
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+######################################## DPEM ########################################
+
+class DynamicConv_Single(nn.Module):
+    """ Dynamic Conv layer
+    """
+    def __init__(self, in_features, out_features, kernel_size=1, stride=1, padding='', dilation=1,
+                 groups=1, bias=False, num_experts=4):  # 可以修改数量
+        super().__init__()
+        self.routing = nn.Linear(in_features, num_experts)
+        self.cond_conv = CondConv2d(in_features, out_features, kernel_size, stride, padding, dilation,
+                 groups, bias, num_experts)
+        
+    def forward(self, x):
+        pooled_inputs = F.adaptive_avg_pool2d(x, 1).flatten(1)  # CondConv routing
+        routing_weights = torch.sigmoid(self.routing(pooled_inputs))
+        x = self.cond_conv(x, routing_weights)
+        return x
+
+class DynamicConv(nn.Module):
+    default_act = nn.SiLU()  # default activation
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True, num_experts=4):  # 可以修改数量
+        super().__init__()
+        self.conv = nn.Sequential(
+            DynamicConv_Single(c1, c2, kernel_size=k, stride=s, padding=autopad(k, p, d), dilation=d, groups=g, num_experts=num_experts),
+            nn.BatchNorm2d(c2),
+            self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        )
+    
+    def forward(self, x):
+        return self.conv(x)
+
+class Bottleneck_DynamicConv(Bottleneck):
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        super().__init__(c1, c2, shortcut, g, k, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.cv2 = DynamicConv(c2, c2, 3)
+
+class DPEM(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(Bottleneck_DynamicConv(self.c, self.c, shortcut, g, k=(3, 3), e=1.0) for _ in range(n))
+
+######################################## DPEM end ########################################
